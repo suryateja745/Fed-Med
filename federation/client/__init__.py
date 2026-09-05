0b6323d03d5be90212ba78fa28ca9167f8c9730e
mohanevs <vaibhavmohane226@gmail.com>
2026-09-05 10:27:02 +0530

    feat(client): add dynamic local dataset path configuration and client CLI runner

--- federation/client/__init__.py
+++ federation/client/__init__.py
@@ -7,9 +7,19 @@ from federation.client.fl_client import (
     create_client,
     start_fedmed_client,
 )
+from federation.client.run_client import (
+    inspect_hardware_environment,
+    main as run_client_main,
+    parse_args as parse_client_args,
+    validate_dataset_directory,
+)
 
 __all__ = [
     "FedMedClient",
     "create_client",
     "start_fedmed_client",
+    "run_client_main",
+    "inspect_hardware_environment",
+    "validate_dataset_directory",
+    "parse_client_args",
 ]
